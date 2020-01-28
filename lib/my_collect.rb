86f69e8c8b925d3aa12a207f873ab2a005df4572
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-01-28 11:30:55 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,16 @@
-def hello(array)
+require 'pry'
+
+def my_collect(content)
   i = 0
-  while i < array.length
-    binding.pry
-    yield(array[i])
+  collection = []
+  while i < content.length
+  collection <<
+    yield(content[i])
     i += 1
   end
+  collection
 end
+
 
 hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
 
